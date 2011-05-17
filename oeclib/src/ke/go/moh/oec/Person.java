4515e23174f3b870d81a6f527f6a525df0285477
Jim Grace <github@jamesgrace.org>
2011-05-17 09:52:29 +0300

    Add shallow clone() to Person.

--- oeclib/src/ke/go/moh/oec/Person.java
+++ oeclib/src/ke/go/moh/oec/Person.java
@@ -34,127 +34,141 @@ import java.util.List;
  *
  * @author Jim Grace
  */
-public class Person {
+public class Person implements Cloneable {
 
-	public enum MaritalStatus {
+    public enum MaritalStatus {
 
-		marriedPolygamous,
-		marriedMonogamous,
-		divorced,
-		widowed,
-		cohabitating
-	}
+        marriedPolygamous,
+        marriedMonogamous,
+        divorced,
+        widowed,
+        cohabitating
+    }
 
-	public enum Sex {
+    public enum Sex {
 
-		F,
-		M
-	}
+        F,
+        M
+    }
 
-	public enum AliveStatus {
+    public enum AliveStatus {
 
-		yes,
-		no
-	}
+        yes,
+        no
+    }
 
-	public enum ConsentSigned {
+    public enum ConsentSigned {
 
-		yes,
-		no,
-		notAnswered
-	}
+        yes,
+        no,
+        notAnswered
+    }
 
-	public enum PregnancyOutcome {
+    public enum PregnancyOutcome {
 
-		stillBirth,
-		singleBirth,
-		multipleBirths
-	}
-	/**
-	 * A Globally Unique IDentifier (GUID) for this person, as stored in the
-	 * person index being referenced.
-	 */
-	private String personGuid;
-	/** A person's first (or given) name. */
-	private String firstName;		// Need from HDSS add/modify
-	/** A person's middle (Luo: juok) name. */
-	private String middleName;		// Need from HDSS add/modify
-	/** A person's last (or family) name. */
-	private String lastName;		// Need from HDSS add/modify
-	/** Any other name by which a person is commonly known. */
-	private String otherName;		 // Need from HDSS add/modify
-	/** The name of the clan to which a person belongs. */
-	private String clanName;		// Need from HDSS add/modify
-	/** The person's gender: M or F. */
-	private Sex sex;		// Need from HDSS add/modify
-	/** The person's date of birth. */
-	private Date birthdate;		// Need from HDSS add/modify
-	/** The person's date of death (if any). */
-	private Date deathdate;		// Need from HDSS add/modify
-	/** Whether a person is living at the moment: 0 = N/A, 1 = Yes, 2 = No (only used as a person search term) */
-	private AliveStatus aliveStatus;
-	/** The first (or given) name of the person's mother. */
-	private String mothersFirstName;		// Need from HDSS add/modify
-	/** The middle (Luo: juok) name of the person's mother. */
-	private String mothersMiddleName;		// Need from HDSS add/modify
-	/** The last (or family) name of the person's mother. */
-	private String mothersLastName;		// Need from HDSS add/modify
-	/** The first (or given) name of the person's father. */
-	private String fathersFirstName;		// Need from HDSS add/modify
-	/** The middle (Luo: juok) name of the person's father. */
-	private String fathersMiddleName;		// Need from HDSS add/modify
-	/** The last (or family) name of the person's father. */
-	private String fathersLastName;		// Need from HDSS add/modify
-	/** The first (or given) name of the head of the person's compound. */
-	private String compoundHeadFirstName;		// Need from HDSS add/modify
-	/** The middle (Luo: juok) name of the head of the person's compound. */
-	private String compoundHeadMiddleName;		// Need from HDSS add/modify
-	/** The last (or family) name of the head of the person's compound. */
-	private String compoundHeadLastName;		// Need from HDSS add/modify
-	/** The name of the site from which a remote visiting patient comes (only used as a person search term.) */
-	private String siteName;
-	/**
-	 * The name of the village in which the person lives.
-	 * If the person moves outside the HDSS area to a known location or area,
-	 * the HDSS should provide here the name of the place to which they moved.
-	 * If the person moves outside the HDSS area to an unknown location,
-	 * the village name should be null.
-	 */
-	private String villageName;		// Need from HDSS add/modify
-	/** The name of the village in which the person previously lived most recently. */
-	private String previousVillageName;
-	/**
-	 * Date of most recent move between villages.
-	 * Only needed from the HDSS when the villageName changes.
-	 */
-	private Date lastMoveDate;		// Need from HDSS modify
-	/** The person's marital status */
-	private MaritalStatus maritalStatus;		// Need from HDSS add/modify
-	/** Has the person given consent for HDSS data to be transfered to clinics? */
-	private ConsentSigned consentSigned;
-	/** Expected Delivery Date if pregnant */
-	private Date expectedDeliveryDate;		// Need from HDSS add/modify
-	/** Date at which the most recent pregnancy was ended */
-	private Date pregnancyEndDate;		// Need from HDSS add/modify
-	/** Most recent pregnancy outcome, one of the following values:
-	 *       Live birth
-	 *       Still birth
-	 */
-	private PregnancyOutcome pregnancyOutcome;		// Need from HDSS add/modify
-	/** Information for the person's most recent regular clinic visit (if any) */
-	private Visit lastRegularVisit;
-	/** Information for the person's most recent one-off clinic visit (if any) */
-	private Visit lastOneOffVisit;
-	/** The score when matching this person in a person/patient index (returns with match results) */
-	private int matchScore;
-	/** A list containing each {@link PersonIdentifier} assigned to this person. */
-	private List<PersonIdentifier> personIdentifierList;		// Need from HDSS add/modify
-	/** A list containing each {@link Fingerprint} taken from this person. */
-	private List<Fingerprint> fingerprintList;		// Need from HDSS add/modify (HDSS ID only)
-	/** A list of household members */
-	private List<RelatedPerson> householdMembers;	// Need on HDSS findPerson query response
+        stillBirth,
+        singleBirth,
+        multipleBirths
+    }
+    /**
+     * A Globally Unique IDentifier (GUID) for this person, as stored in the
+     * person index being referenced.
+     */
+    private String personGuid;
+    /** A person's first (or given) name. */
+    private String firstName;		// Need from HDSS add/modify
+    /** A person's middle (Luo: juok) name. */
+    private String middleName;		// Need from HDSS add/modify
+    /** A person's last (or family) name. */
+    private String lastName;		// Need from HDSS add/modify
+    /** Any other name by which a person is commonly known. */
+    private String otherName;		 // Need from HDSS add/modify
+    /** The name of the clan to which a person belongs. */
+    private String clanName;		// Need from HDSS add/modify
+    /** The person's gender: M or F. */
+    private Sex sex;		// Need from HDSS add/modify
+    /** The person's date of birth. */
+    private Date birthdate;		// Need from HDSS add/modify
+    /** The person's date of death (if any). */
+    private Date deathdate;		// Need from HDSS add/modify
+    /** Whether a person is living at the moment: 0 = N/A, 1 = Yes, 2 = No (only used as a person search term) */
+    private AliveStatus aliveStatus;
+    /** The first (or given) name of the person's mother. */
+    private String mothersFirstName;		// Need from HDSS add/modify
+    /** The middle (Luo: juok) name of the person's mother. */
+    private String mothersMiddleName;		// Need from HDSS add/modify
+    /** The last (or family) name of the person's mother. */
+    private String mothersLastName;		// Need from HDSS add/modify
+    /** The first (or given) name of the person's father. */
+    private String fathersFirstName;		// Need from HDSS add/modify
+    /** The middle (Luo: juok) name of the person's father. */
+    private String fathersMiddleName;		// Need from HDSS add/modify
+    /** The last (or family) name of the person's father. */
+    private String fathersLastName;		// Need from HDSS add/modify
+    /** The first (or given) name of the head of the person's compound. */
+    private String compoundHeadFirstName;		// Need from HDSS add/modify
+    /** The middle (Luo: juok) name of the head of the person's compound. */
+    private String compoundHeadMiddleName;		// Need from HDSS add/modify
+    /** The last (or family) name of the head of the person's compound. */
+    private String compoundHeadLastName;		// Need from HDSS add/modify
+    /** The name of the site from which a remote visiting patient comes (only used as a person search term.) */
+    private String siteName;
+    /**
+     * The name of the village in which the person lives.
+     * If the person moves outside the HDSS area to a known location or area,
+     * the HDSS should provide here the name of the place to which they moved.
+     * If the person moves outside the HDSS area to an unknown location,
+     * the village name should be null.
+     */
+    private String villageName;		// Need from HDSS add/modify
+    /** The name of the village in which the person previously lived most recently. */
+    private String previousVillageName;
+    /**
+     * Date of most recent move between villages.
+     * Only needed from the HDSS when the villageName changes.
+     */
+    private Date lastMoveDate;		// Need from HDSS modify
+    /** The person's marital status */
+    private MaritalStatus maritalStatus;		// Need from HDSS add/modify
+    /** Has the person given consent for HDSS data to be transfered to clinics? */
+    private ConsentSigned consentSigned;
+    /** Expected Delivery Date if pregnant */
+    private Date expectedDeliveryDate;		// Need from HDSS add/modify
+    /** Date at which the most recent pregnancy was ended */
+    private Date pregnancyEndDate;		// Need from HDSS add/modify
+    /** Most recent pregnancy outcome, one of the following values:
+     *       Live birth
+     *       Still birth
+     */
+    private PregnancyOutcome pregnancyOutcome;		// Need from HDSS add/modify
+    /** Information for the person's most recent regular clinic visit (if any) */
+    private Visit lastRegularVisit;
+    /** Information for the person's most recent one-off clinic visit (if any) */
+    private Visit lastOneOffVisit;
+    /** The score when matching this person in a person/patient index (returns with match results) */
+    private int matchScore;
+    /** A list containing each {@link PersonIdentifier} assigned to this person. */
+    private List<PersonIdentifier> personIdentifierList;		// Need from HDSS add/modify
+    /** A list containing each {@link Fingerprint} taken from this person. */
+    private List<Fingerprint> fingerprintList;		// Need from HDSS add/modify (HDSS ID only)
+    /** A list of household members */
+    private List<RelatedPerson> householdMembers;	// Need on HDSS findPerson query response
     /** Used on a findPerson response: there was a fingerprint match for this candidate. */
     private boolean fingerprintMatched;
+
+    /**
+     * Does a shallow clone of a Person object.
+     * @return the cloned Person object.
+     */
+    @Override
+    public Person clone() {
+        try {
+            return (Person) super.clone();
+        } catch (CloneNotSupportedException e) {
+            // This should never happen
+            throw new InternalError(e.toString());
+        }
+    }
 
     public AliveStatus getAliveStatus() {
         return aliveStatus;
@@ -435,5 +449,4 @@ public class Person {
     public void setVillageName(String villageName) {
         this.villageName = villageName;
     }
-
 }
