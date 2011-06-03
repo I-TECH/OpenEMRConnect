3244ec9cea4b579fa239b6c3c644dac513c3a8e8
Jim Grace <github@jamesgrace.org>
2011-06-03 11:23:32 +0300

    Add xml string to PersonRequest

--- oeclib/src/ke/go/moh/oec/PersonRequest.java
+++ oeclib/src/ke/go/moh/oec/PersonRequest.java
@@ -37,16 +37,16 @@ public class PersonRequest {
      * A request reference number. This is not
      * supplied by the client when making a new request.
      * But it is always supplied by the library to the server when the
-	 * request is delivered to the server. It may also be specified by the caller
-	 * in order to associate a request with an earlier request.
-	 * <p>
-	 * For example, a client would specify the reference number from
-	 * a prior person search when making a subsequent call relating
-	 * to the same search. It is also supplied by the client when taking
-	 * action such as add person or modify person, based on the results
-	 * of a previous find person. This lets the server know which person
-	 * was chosen (or, in the event of ADD PERSON, that no person was
-	 * chosen from the search results.)
+     * request is delivered to the server. It may also be specified by the caller
+     * in order to associate a request with an earlier request.
+     * <p>
+     * For example, a client would specify the reference number from
+     * a prior person search when making a subsequent call relating
+     * to the same search. It is also supplied by the client when taking
+     * action such as add person or modify person, based on the results
+     * of a previous find person. This lets the server know which person
+     * was chosen (or, in the event of ADD PERSON, that no person was
+     * chosen from the search results.)
      */
     private String requestReference;
     /**
@@ -65,56 +65,70 @@ public class PersonRequest {
      * the requester may specify where to send the revised person data.
      */
     private String destinationName;
-	/**
-	 * Match algorithm to use. One of the following values:
-	 * <p>
-	 * Normal - Do a normal, weighted match <br>
-	 * FingerprintMustMatch - Like normal except only return results where
-	 * fingerprint is above the fingerprint match confidence threshold.
-	 * <p>
-	 * If not specified, the default is Normal.
-	 */
-	private String matchAlgorithm;
+    /**
+     * Match algorithm to use. One of the following values:
+     * <p>
+     * Normal - Do a normal, weighted match <br>
+     * FingerprintMustMatch - Like normal except only return results where
+     * fingerprint is above the fingerprint match confidence threshold.
+     * <p>
+     * If not specified, the default is Normal.
+     */
+    private String matchAlgorithm;
+    /**
+     * The message in XML form.
+     * <p>
+     * This can be used to deliver the message to a destination in raw, XML form.
+     * It may also be used to send a pre-formatted XML message to a destination.
+     * Otherwise this value is not used.
+     */
+    private String xml;
 
-	public String getDestinationAddress() {
-		return destinationAddress;
-	}
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
 
-	public void setDestinationAddress(String destinationAddress) {
-		this.destinationAddress = destinationAddress;
-	}
+    public void setDestinationAddress(String destinationAddress) {
+        this.destinationAddress = destinationAddress;
+    }
 
-	public String getDestinationName() {
-		return destinationName;
-	}
+    public String getDestinationName() {
+        return destinationName;
+    }
 
-	public void setDestinationName(String destinationName) {
-		this.destinationName = destinationName;
-	}
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
 
-	public String getMatchAlgorithm() {
-		return matchAlgorithm;
-	}
+    public String getMatchAlgorithm() {
+        return matchAlgorithm;
+    }
 
-	public void setMatchAlgorithm(String matchAlgorithm) {
-		this.matchAlgorithm = matchAlgorithm;
-	}
+    public void setMatchAlgorithm(String matchAlgorithm) {
+        this.matchAlgorithm = matchAlgorithm;
+    }
 
-	public Person getPerson() {
-		return person;
-	}
+    public Person getPerson() {
+        return person;
+    }
 
-	public void setPerson(Person person) {
-		this.person = person;
-	}
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 
-	public String getRequestReference() {
-		return requestReference;
-	}
+    public String getRequestReference() {
+        return requestReference;
+    }
 
-	public void setRequestReference(String requestReference) {
-		this.requestReference = requestReference;
-	}
+    public void setRequestReference(String requestReference) {
+        this.requestReference = requestReference;
+    }
 
-	
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(String xml) {
+        this.xml = xml;
+    }
 }
