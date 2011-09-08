697bef394bfcbdae79ea52170982dfff7618e3ee
Gitahi Ng'ang'a <gitahi86@gmail.com>
2011-09-08 12:28:46 +0300

    Fix fingerprint memory leak

--- mpi/src/ke/go/moh/oec/mpi/FindPersonThread.java
+++ mpi/src/ke/go/moh/oec/mpi/FindPersonThread.java
@@ -121,5 +121,11 @@ public class FindPersonThread implements Runnable {
                 candidateSet.add(pm, s);
             }
         }
+        
+        // Destroy the matching context for each fingerprint in our search list.
+        
+        for (FingerprintMatch f : fSearchList) {
+            f.destroy();
+        }
     }
 }
