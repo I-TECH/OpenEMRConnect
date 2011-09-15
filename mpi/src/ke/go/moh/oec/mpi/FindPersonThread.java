cd07f74e6dabe622459784ef700d8ce9430f8009
Gitahi Ng'ang'a <gitahi86@gmail.com>
2011-09-15 15:11:43 +0300

    Added password resupply requirement for admin before making changes, hidding menu items which the current user is not allowed to use. Included support for multiple GUI themes.

--- mpi/src/ke/go/moh/oec/mpi/FindPersonThread.java
+++ mpi/src/ke/go/moh/oec/mpi/FindPersonThread.java
@@ -121,12 +121,8 @@ public class FindPersonThread implements Runnable {
                 candidateSet.add(pm, s);
             }
         }
-<<<<<<< HEAD
-        if (fSearchList != null) {
-=======
-        if (fSearchList != null &&
-                !fSearchList.isEmpty()) {
->>>>>>> gitahi86/master
+        if (fSearchList != null
+                && !fSearchList.isEmpty()) {
             for (FingerprintMatch f : fSearchList) {
                 f.destroy();
             }
