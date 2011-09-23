22da1ee1d9f1a3b954327c7fe2915e173f427eb0
Jim Grace <github@jamesgrace.org>
2011-09-23 11:22:59 +0300

    If multiple notifications, send one at a time

--- mpi/src/ke/go/moh/oec/mpi/Notifier.java
+++ mpi/src/ke/go/moh/oec/mpi/Notifier.java
@@ -48,33 +48,79 @@ public class Notifier {
      * with application address ke.go.moh.facility.14080.tb.reception.
      * The notification will be sent to the Clinical Document Store at address
      * ke.go.moh.facility.14080.cds.
+     * <p>
+     * If there is more than one reason to notify about a patient update,
+     * then send a different notification message for each reason.
      * 
      * @param p updated person
      */
     public static void notify(Person p) {
-        if (p.getLastMoveDate() != null
-                || p.getExpectedDeliveryDate() != null
-                || p.getPregnancyEndDate() != null
-                || p.getDeathdate() != null) {
-            Visit v = p.getLastRegularVisit();
-            if (v != null) {
-                String address = v.getAddress();
-                if (address != null) {
-                    String facility = address.substring(0, address.indexOf(".facility.") + 15);
-                    String destination = facility + ".cds";
-                    PersonRequest pr = new PersonRequest();
-                    pr.setPerson(p);
-                    pr.setDestinationAddress(destination);
-                    pr.setDestinationName("Clinical Document Store");
-                    Mediator mediator = Main.getMediator();
-                    mediator.getData(RequestTypeId.NOTIFY_PERSON_CHANGED, pr);
-                    //reset notification fields to avoid recurrent notification
+        Visit v = p.getLastRegularVisit();
+        if (v != null) {
+            String address = v.getAddress();
+            if (address != null) {
+                if (p.getLastMoveDate() != null) {
+                    Person per = p.clone();
+                    // Remove any other possible alert data, so this alert is for one purpose only:
+                    per.setExpectedDeliveryDate(null);
+                    per.setPregnancyEndDate(null);
+                    per.setDeathdate(null);
+                    // Send the alert (notification):
+                    sendNotify(per, address);
+                    // Remove this data from our in-memory person, so we don't keep sending the same alert next time.
                     p.setLastMoveDate(null);
+                }
+                if (p.getExpectedDeliveryDate() != null) {
+                    Person per = p.clone();
+                    // Remove any other possible alert data, so this alert is for one purpose only:
+                    per.setLastMoveDate(null);
+                    per.setPregnancyEndDate(null);
+                    per.setDeathdate(null);
+                    // Send the alert (notification):
+                    sendNotify(per, address);
+                    // Remove this data from our in-memory person, so we don't keep sending the same alert next time.
                     p.setExpectedDeliveryDate(null);
+                }
+                if (p.getPregnancyEndDate() != null) {
+                    Person per = p.clone();
+                    // Remove any other possible alert data, so this alert is for one purpose only:
+                    per.setLastMoveDate(null);
+                    per.setExpectedDeliveryDate(null);
+                    per.setDeathdate(null);
+                    // Send the alert (notification):
+                    sendNotify(per, address);
+                    // Remove this data from our in-memory person, so we don't keep sending the same alert next time.
                     p.setPregnancyEndDate(null);
+                }
+                if (p.getDeathdate() != null) {
+                    Person per = p.clone();
+                    // Remove any other possible alert data, so this alert is for one purpose only:
+                    per.setLastMoveDate(null);
+                    per.setExpectedDeliveryDate(null);
+                    per.setPregnancyEndDate(null);
+                    // Send the alert (notification):
+                    sendNotify(per, address);
+                    // Remove this data from our in-memory person, so we don't keep sending the same alert next time.
                     p.setDeathdate(null);
                 }
             }
         }
+    }
+
+    /**
+     * Sends a notify message to the appropriate Clinical Document Store (CDS)
+     * 
+     * @param per Person including the field to notify.
+     * @param address Address of the reception system where the person was last seen for a regular visit.
+     */
+    private static void sendNotify(Person per, String address) {
+        String facility = address.substring(0, address.indexOf(".facility.") + 15);
+        String destination = facility + ".cds";
+        PersonRequest pr = new PersonRequest();
+        pr.setPerson(per);
+        pr.setDestinationAddress(destination);
+        pr.setDestinationName("Clinical Document Store");
+        Mediator mediator = Main.getMediator();
+        mediator.getData(RequestTypeId.NOTIFY_PERSON_CHANGED, pr);
     }
 }
