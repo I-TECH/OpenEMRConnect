32259e5ff4dac67ffcf6c46552b71780aef05743
Jim Grace <github@jamesgrace.org>
2011-11-03 10:20:22 +0300

    Add MD5 hash and retrying to HttpService

--- oeclib/src/ke/go/moh/oec/lib/HttpService.java
+++ oeclib/src/ke/go/moh/oec/lib/HttpService.java
@@ -36,20 +36,23 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.OutputStream;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.HttpURLConnection;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -69,8 +72,11 @@ class HttpService {
     private int id = 0;
     private int port = 0;
     HttpServer server;
+    MessageDigest messageDigest;
     Map<String, Date> unreachableIpPorts = new HashMap<String, Date>();
     private static final SimpleDateFormat SIMPLE_DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    private static final int HTTP_RESPONSE_OK = 200;
+    private static final int HTTP_RESPONSE_MD5_MISMATCH = 449; // No obvious choice here, this code is Microsoft "Retry With"
 
     private class PartialMessage {
 
@@ -88,6 +94,11 @@ class HttpService {
      */
     HttpService(Mediator mediator) {
         this.mediator = mediator;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(HttpService.class.getName()).log(Level.SEVERE, "Can't get an instance of the MD5 algorithm", ex);
+        }
     }
 
     /**
@@ -135,21 +146,37 @@ class HttpService {
                         thisUrl = thisUrl + "&end";
                     }
                 }
-                URLConnection connection = new URL(thisUrl).openConnection();
+                HttpURLConnection connection = (HttpURLConnection) new URL(thisUrl).openConnection();
+                String md5 = computeMd5(compressedXml, sent, toSend);
+                connection.setRequestProperty("Content-MD5", md5);
                 connection.setDoOutput(true);
                 OutputStream output = connection.getOutputStream();
 
                 output.write(compressedXml, sent, toSend);
-                sent = sent + toSend;
                 output.close();
-
-                InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
-                BufferedReader br = new BufferedReader(inputStreamReader);
-                while (br.readLine() != null) {
-                    //content not required, just acknowlegment that message was received.
+                int responseCode = connection.getResponseCode();
+                //
+                // If we get a HTTP_RESPONSE_MD5_MISMATCH from the other side,
+                // then just keep retrying to send the same message over and over.
+                // As long as something is getting through, then the whole message should go through.
+                //
+                // If we get any other kind of response, account for the number of bytes
+                // sent, and continue sending (or finish if everything was sent.)
+                //
+                if (responseCode != HTTP_RESPONSE_MD5_MISMATCH) {
+                    sent = sent + toSend;
+                    InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+                    BufferedReader br = new BufferedReader(inputStreamReader);
+                    while (br.readLine() != null) {
+                        //content not required, just acknowlegment that message was received.
+                    }
+                    br.close();
+                    inputStreamReader.close();
+                } else {
+                    Logger.getLogger(HttpService.class.getName()).log(Level.FINER,
+                            "MD5 mismatch reported. Retrying sending message to {0} at {1}",
+                            new Object[]{m.getDestinationAddress(), url});
                 }
-                br.close();
-                inputStreamReader.close();
             }
             returnStatus = true;
             canReach(ipAddressPort);
@@ -305,68 +332,87 @@ class HttpService {
                  * Read the posted content
                  */
                 Headers headers = exchange.getRequestHeaders();
+                int responseCode = HTTP_RESPONSE_OK;
+                boolean outOfSequence = false;
+                int bufferSize = 50000; // Default buffer size if no Content-Length header is present.
+                String contentLength = headers.getFirst("Content-Length");
+                if (contentLength != null) {
+                    bufferSize = Integer.parseInt(contentLength);
+                }
                 InputStream input = exchange.getRequestBody();
-                byte[] compressedXml = new byte[50000];
+                byte[] compressedXml = new byte[bufferSize];
                 int compressedXmlLength = input.read(compressedXml);
                 input.close();
-                InetSocketAddress remoteAddress = exchange.getRemoteAddress();
-                String sendingIpAddress = remoteAddress.getAddress().getHostAddress();
-                boolean completeMessage = true;
-                boolean outOfSequence = false;
-                m.setSendingIpAddress(sendingIpAddress);
-                m.setSegmentCount(1);
-                m.setLongestSegmentLength(compressedXmlLength);
-                if (id > 0) {
-                    completeMessage = false;
-                    String sendingIpAddressAndPort = sendingIpAddress + ":" + port;
-                    PartialMessage pm = null;
-                    if (segment == 1) {
-                        pm = new PartialMessage();
-                        pm.id = id;
-                        pm.segment = segment;
-                        byte[] a = Arrays.copyOf(compressedXml, compressedXmlLength);
-                        pm.messageSegments.add(a);
-                        pm.length += compressedXmlLength;
-                        partialMessages.put(sendingIpAddressAndPort, pm);
-                    } else {
-                        pm = partialMessages.get(sendingIpAddressAndPort);
-                        if (pm != null) {
-                            if (pm.id == id && ++pm.segment == segment) {
-                                byte[] a = Arrays.copyOf(compressedXml, compressedXmlLength);
-                                pm.messageSegments.add(a);
-                                pm.length += compressedXmlLength;
-                                if (end) {
-                                    compressedXmlLength = pm.length;
-                                    compressedXml = new byte[compressedXmlLength];
-                                    int offset = 0;
-                                    int longest = 0;
-                                    for (byte[] seg : pm.messageSegments) {
-                                        System.arraycopy(seg, 0, compressedXml, offset, seg.length);
-                                        offset += seg.length;
-                                        if (seg.length > longest) {
-                                            longest = seg.length;
+                String md5Reported = headers.getFirst("Content-MD5");
+                if (md5Reported != null) {
+                    String md5Computed = computeMd5(compressedXml, 0, compressedXmlLength);
+                    if (md5Reported.compareTo(md5Computed) != 0) {
+                        responseCode = HTTP_RESPONSE_MD5_MISMATCH;
+                        Logger.getLogger(HttpService.class.getName()).log(Level.FINER,
+                                "MD5 reported as {0}, computed as {1}, length expected {2}, found {3}",
+                                new Object[]{md5Reported, md5Computed, bufferSize, compressedXmlLength});
+                    }
+                }
+                if (responseCode == HTTP_RESPONSE_OK) {
+                    responseCode = HTTP_RESPONSE_OK;
+                    InetSocketAddress remoteAddress = exchange.getRemoteAddress();
+                    String sendingIpAddress = remoteAddress.getAddress().getHostAddress();
+                    boolean completeMessage = true;
+                    m.setSendingIpAddress(sendingIpAddress);
+                    m.setSegmentCount(1);
+                    m.setLongestSegmentLength(compressedXmlLength);
+                    if (id > 0) {
+                        completeMessage = false;
+                        String sendingIpAddressAndPort = sendingIpAddress + ":" + port;
+                        PartialMessage pm = null;
+                        if (segment == 1) {
+                            pm = new PartialMessage();
+                            pm.id = id;
+                            pm.segment = segment;
+                            byte[] a = Arrays.copyOf(compressedXml, compressedXmlLength);
+                            pm.messageSegments.add(a);
+                            pm.length += compressedXmlLength;
+                            partialMessages.put(sendingIpAddressAndPort, pm);
+                        } else {
+                            pm = partialMessages.get(sendingIpAddressAndPort);
+                            if (pm != null) {
+                                if (pm.id == id && ++pm.segment == segment) {
+                                    byte[] a = Arrays.copyOf(compressedXml, compressedXmlLength);
+                                    pm.messageSegments.add(a);
+                                    pm.length += compressedXmlLength;
+                                    if (end) {
+                                        compressedXmlLength = pm.length;
+                                        compressedXml = new byte[compressedXmlLength];
+                                        int offset = 0;
+                                        int longest = 0;
+                                        for (byte[] seg : pm.messageSegments) {
+                                            System.arraycopy(seg, 0, compressedXml, offset, seg.length);
+                                            offset += seg.length;
+                                            if (seg.length > longest) {
+                                                longest = seg.length;
+                                            }
                                         }
+                                        m.setSegmentCount(pm.messageSegments.size());
+                                        m.setLongestSegmentLength(longest);
+                                        completeMessage = true;
+                                        partialMessages.remove(sendingIpAddressAndPort);
                                     }
-                                    m.setSegmentCount(pm.messageSegments.size());
-                                    m.setLongestSegmentLength(longest);
-                                    completeMessage = true;
+                                } else {
+                                    outOfSequence = true;
                                     partialMessages.remove(sendingIpAddressAndPort);
                                 }
-                            } else {
-                                outOfSequence = true;
-                                partialMessages.remove(sendingIpAddressAndPort);
                             }
                         }
                     }
-                }
-                if (completeMessage) {
-                    m.setSendingIpAddress(sendingIpAddress);
-                    m.setCompressedXml(compressedXml);
-                    m.setCompressedXmlLength(compressedXmlLength);
-                    /*
-                     * Process the message.
-                     */
-                    mediator.processReceivedMessage(m);
+                    if (completeMessage) {
+                        m.setSendingIpAddress(sendingIpAddress);
+                        m.setCompressedXml(compressedXml);
+                        m.setCompressedXmlLength(compressedXmlLength);
+                        /*
+                         * Process the message.
+                         */
+                        mediator.processReceivedMessage(m);
+                    }
                 }
                 if (!outOfSequence) {
                     /*
@@ -382,5 +428,26 @@ class HttpService {
                 exchange.close();
             }
         }
+    }
+
+    /**
+     * Computes the MD5 hash for an array of bytes.
+     * 
+     * @param bytes array of bytes for which the MD5 hash will be computed
+     * @param offset starting offset for computing the MD5 hasn
+     * @param length length for computing the MD5 hash
+     * @return the MD5 hash in 32 characters hexadecimal.
+     */
+    String computeMd5(byte[] bytes, int offset, int length) {
+        messageDigest.reset();
+        messageDigest.update(bytes, offset, length);
+        byte[] digest = messageDigest.digest();
+        BigInteger bigInt = new BigInteger(1, digest);
+        String hashtext = bigInt.toString(16);
+        // Now we need to zero pad it to get the full 32 chars.
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
     }
 }
