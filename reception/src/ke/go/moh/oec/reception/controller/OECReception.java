931002efc4e5a719b2d995e333226a7b66a2aa3a
Gitahi Ng'ang'a <gitahi86@gmail.com>
2011-12-23 09:15:10 +0300

    Commit to fix unexpected conflicts.

--- reception/src/ke/go/moh/oec/reception/controller/OECReception.java
+++ reception/src/ke/go/moh/oec/reception/controller/OECReception.java
@@ -70,7 +70,6 @@ public class OECReception {
         }
         return instanceName;
     }
-<<<<<<< HEAD
 
     public static String facilityName() {
         String instanceName = Mediator.getProperty("Instance.FacilityName");
@@ -88,17 +87,6 @@ public class OECReception {
         return instanceName;
     }
 
-=======
-    
-    public static String facilityName(){
-        String instanceName = Mediator.getProperty("Instance.FacilityName");
-        if (instanceName == null){
-            instanceName = "OEC Reception";
-        }
-    return instanceName;
-    }
-    
->>>>>>> gitahi86/master
     public static String generateSessionReference() {
         return Mediator.generateMessageId();
     }
