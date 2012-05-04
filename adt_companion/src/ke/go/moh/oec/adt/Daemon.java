a6d0f19bdb5619afa49a09dbc21bb5851bf83e0f
Jim Sibley <jsibley@uw.edu>
2012-05-04 16:49:44 -0700

    Add code to ADT companion to send extract files via HTTP to a remote Mirth instance.

--- adt_companion/src/ke/go/moh/oec/adt/Daemon.java
+++ adt_companion/src/ke/go/moh/oec/adt/Daemon.java
@@ -24,7 +24,12 @@
  * ***** END LICENSE BLOCK ***** */
 package ke.go.moh.oec.adt;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
@@ -40,6 +45,14 @@ import ke.go.moh.oec.adt.data.Transaction;
 import ke.go.moh.oec.adt.exceptions.BadRecordSourceException;
 import ke.go.moh.oec.adt.format.OneLineRecordFormat;
 import ke.go.moh.oec.adt.format.RecordFormat;
+import org.apache.commons.codec.binary.Base64InputStream;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpConnectionManager;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
 import org.xml.sax.SAXException;
 
 /**
@@ -76,7 +89,22 @@ public class Daemon implements Runnable {
                 if (!linkedRecordList.isEmpty()) {
                     RecordFormat oneLineFormat = new OneLineRecordFormat();
                     RecordCsvWriter csvWriter = new RecordCsvWriter(oneLineFormat);
-                    csvWriter.writeToCsv(linkedRecordList, "Extract No. " + new Date().getTime());
+                    String filename = "ADT Extract No. " + new Date().getTime();
+                    csvWriter.writeToCsv(linkedRecordList, filename);
+
+                    // Send extracted file to remote Mirth instance if so configured
+                    if ("remote".equalsIgnoreCase(ResourceManager.getSetting("mirth.location"))) {
+           				if (!"".equals(ResourceManager.getSetting("mirth.url")) &&
+           					ResourceManager.getSetting("mirth.url") != null) {
+            				if (sendMessage(ResourceManager.getSetting("mirth.url"), filename + ".csv")) {
+            					Logger.getLogger(Daemon.class.getName()).log(Level.INFO, "File sent!");
+            				} else {
+            					Logger.getLogger(Daemon.class.getName()).log(Level.INFO, "File not sent!");
+            				}
+            			} else {
+            				Logger.getLogger(Daemon.class.getName()).log(Level.INFO, "No URL provided for remote Mirth instance.  The file was not sent!");
+            			}
+                    }
                 }
                 transactionMiner.saveLastTransactionId();
                 Thread.sleep(snooze);
@@ -84,15 +112,58 @@ public class Daemon implements Runnable {
         } catch (InterruptedException ex) {
             Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, null, ex);
         } catch (BadRecordSourceException ex) {
-            Logger.getLogger(TransactionMiner.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ParserConfigurationException ex) {
-            Logger.getLogger(TransactionMiner.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SAXException ex) {
-            Logger.getLogger(TransactionMiner.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            Logger.getLogger(TransactionMiner.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(TransactionMiner.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+	private static boolean sendMessage(String url, String filename) {
+		int returnStatus = HttpStatus.SC_CREATED;
+		HttpClient httpclient = new HttpClient();
+		HttpConnectionManager connectionManager = httpclient.getHttpConnectionManager();
+		connectionManager.getParams().setSoTimeout(120000);
+
+		PostMethod httpPost = new PostMethod(url);
+		
+		RequestEntity requestEntity = null;
+		try {
+			FileInputStream message = new FileInputStream(filename);
+			Base64InputStream message64 = new Base64InputStream(message, true, -1, null);
+			requestEntity = new InputStreamRequestEntity(message64, "application/octet-stream");
+		} catch (FileNotFoundException e) {
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, "File not found.", e);
+            return false;
+		}
+		httpPost.setRequestEntity(requestEntity);
+
+		try {
+			httpclient.executeMethod(httpPost);
+			returnStatus = httpPost.getStatusCode();
+		} catch (SocketTimeoutException e) {
+			returnStatus = HttpStatus.SC_REQUEST_TIMEOUT;
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, "Request timed out.  Not retrying.", e);
+		} catch (HttpException e) {
+			returnStatus = HttpStatus.SC_INTERNAL_SERVER_ERROR;
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, "HTTP exception.  Not retrying.", e);
+		} catch (ConnectException e) {
+			returnStatus = HttpStatus.SC_SERVICE_UNAVAILABLE;
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, "Service unavailable.  Not retrying.", e);
+		} catch (UnknownHostException e) {
+			returnStatus = HttpStatus.SC_NOT_FOUND;
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, "Not found.  Not retrying.", e);
+		} catch (IOException e) {
+			returnStatus = HttpStatus.SC_GATEWAY_TIMEOUT;
+            Logger.getLogger(Daemon.class.getName()).log(Level.SEVERE, "IO exception.  Not retrying.", e);
+		} finally {
+			httpPost.releaseConnection();
+		}
+		return returnStatus == HttpStatus.SC_OK;
+	}
 }
